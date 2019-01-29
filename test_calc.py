0dac6b841760270fce65764106bd8f7e7a3c70b5
solyanik <solyanik@avilex.ru>
2019-01-29 18:45:17 +0300

    Initial commit

---
+++ test_calc.py
@@ -0,0 +1,14 @@
+"""
+Unit tests for the calculator library
+"""
+
+import calc
+
+
+class TestCalculator:
+
+    def test_addition(self):
+        assert 4 == calc.add(2, 2)
+
+    def test_subtraction(self):
+        assert 2 == calc.subtract(4, 2)
